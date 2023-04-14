fe6beeeabfb0ca199c0913082cbfea4c2fe52b2d
Nobos Software <nobosoftware@gmail.com>
2023-04-14 18:37:28 -0400

    Initial Commit

---
+++ Brewies/BrewiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrewiesApp.swift
+//  Brewies
+//
+//  Created by Noah Boyers on 4/14/23.
+//
+
+import SwiftUI
+
+@main
+struct BrewiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
