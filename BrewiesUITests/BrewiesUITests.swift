5def8d1f453eb845f6007dccc643b464d80cb1db
Nobos Software <nobosoftware@gmail.com>
2023-06-06 20:12:26 -0400

    Created Performance Unit Tests

--- BrewiesUITests/BrewiesUITests.swift
+++ BrewiesUITests/BrewiesUITests.swift
@@ -9,25 +9,23 @@ import XCTest
 
 final class BrewiesUITests: XCTestCase {
 
+    let app = XCUIApplication()
+
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+        // This setup will be done before each test method is invoked.
+        app.launchArguments += ["UI-Testing"]
+        app.launch()
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        // This teardown will be done after each test method is invoked.
+        app.terminate()
     }
 
     func testExample() throws {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
-        app.launch()
-
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        // Write any specific UI tests here. For example, you can test if the first screen is displaying the correct elements:
+        XCTAssertTrue(app.buttons["Search Area"].exists) // Replace "SomeButton" with the accessibility identifier of a button in your app.
     }
 
     func testLaunchPerformance() throws {
