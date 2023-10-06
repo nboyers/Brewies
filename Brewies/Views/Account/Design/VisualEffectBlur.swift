93722aca1bff40f47a7bcd4ca674dffdb165fc5f
Nobos Software <nobosoftware@gmail.com>
2023-10-06 19:12:34 -0400

    Improved UI by adding an alert for non-subscribed users attempting to access filter options.

---
+++ Brewies/Views/Account/Design/VisualEffectBlur.swift
@@ -0,0 +1,18 @@
+//
+//  VisualEffectBlur.swift
+//  Brewies
+//
+//  Created by Noah Boyers on 10/6/23.
+//
+
+import SwiftUI
+
+struct VisualEffectBlur: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    VisualEffectBlur()
+}
